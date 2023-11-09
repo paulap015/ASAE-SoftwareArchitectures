6745deb41a43f35dcfe211c0d44c207ad7cda701
paula pc <ppena@unicauca.edu.co>
2023-11-09 17:30:45 -0500

    ligados en jpa

---
+++ ejemplo_ligados_jpa/src/main/java/co/edu/unicauca/asae/ejemplo_relaciones_jpa/models/Direccion.java
@@ -0,0 +1,60 @@
+package co.edu.unicauca.asae.ejemplo_relaciones_jpa.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="Direcciones")
+public class Direccion {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String calle;
+	private String ciudad;
+	private String pais;
+		
+	@OneToOne
+	@JoinColumn(name = "idUsuario")
+	private Usuario objUsuario;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public Usuario getObjUsuario() {
+		return objUsuario;
+	}
+	public void setObjUsuario(Usuario objUsuario) {
+		this.objUsuario = objUsuario;
+	}
+	
+	
+	
+}
---
+++ ejemplo_ligados_jpa/src/main/java/co/edu/unicauca/asae/ejemplo_relaciones_jpa/models/Usuario.java
@@ -0,0 +1,121 @@
+package co.edu.unicauca.asae.ejemplo_relaciones_jpa.models;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Usuarios")
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(nullable = false, length = 45)
+	private String username;
+	@Column(name="nombre", nullable = false, length = 45)
+	private String nombreUsuario;
+	@Column( unique=true, nullable = false, length = 100)
+	private String email;
+	private String password;
+	private Integer estatus;
+	@Column( nullable = true)	
+	private Date fechaRegistro;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy="objUsuario")
+	private List<Solicitud> solicitudes;
+		
+	
+	//@OneToOne(mappedBy = "objUsuario")
+	//private Direccion objDireccion;
+
+
+	public Usuario()
+	{		
+		this.solicitudes=new ArrayList<Solicitud>();
+	}
+
+	public void agregarSolicitud(Solicitud objSolicitud)
+	{
+		this.solicitudes.add(objSolicitud);
+	}
+
+	public List<Solicitud> getSolicitudes()
+	{
+		return this.solicitudes;
+	}
+	
+		
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getNombre() {
+		return nombreUsuario;
+	}
+	public void setNombre(String nombre) {
+		this.nombreUsuario = nombre;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Integer getEstatus() {
+		return estatus;
+	}
+	public void setEstatus(Integer estatus) {
+		this.estatus = estatus;
+	}
+	public Date getFechaRegistro() {
+		return fechaRegistro;
+	}
+	public void setFechaRegistro(Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+	/*
+	public Direccion getObjDireccion() {
+		return objDireccion;
+	}
+	public void setObjDireccion(Direccion objDireccion) {
+		this.objDireccion = objDireccion;
+	}
+	*/
+
+	/*
+	public List<Perfil> getListaPerfiles() {
+		return listaPerfiles;
+	}
+	public void setListaPerfiles(List<Perfil> listaPerfiles) {
+		this.listaPerfiles = listaPerfiles;
+	}*/
+	
+	
+}
